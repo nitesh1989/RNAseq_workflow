730fed47a69624326c77056ab4d60a6192c5f348
nitesh1989 <nturaga@andrew.cmu.edu>
2014-02-12 16:51:11 -0500

    join cancer data

--- 02_explore.R
+++ 02_explore.R
@@ -10,7 +10,14 @@ sample = "BRCA"
 my.path = file.path("~/TestRun/TCGA-Expression-Gene",sample)
 setwd(my.path)
 
-dataDir = file.path(sample,"data")
+# Set data directory
+dataDir ="data"
 
-files = list.files(dataDir,full.names=TRUE,recursive=T)
-sdad
+# list cancer and normal files
+cancer = list.files(file.path(dataDir,"Cancer"),full.names=TRUE,recursive=T)
+normal = list.files(file.path(dataDir,"Normal"),full.names=TRUE,recursive=T)
+
+# Read in data
+
+f1 = read.delim(cancer[1],header =T,stringsAsFactors =FALSE,sep="\t")
+cancer_dat = cbind(f1,sappl)
