f60fc70b95fd9eb4372e8574817a490a3294c505
nitesh1989 <nturaga@andrew.cmu.edu>
2014-02-12 15:37:56 -0500

    Explore UNC Agilent Expression Gene data. Make plots.

---
+++ 02_explore.R
@@ -0,0 +1,15 @@
+# Nitesh Turaga
+# TCGA- Expression-Gene UNC Agilent analysis
+
+library(plyr)
+library(reshape)
+
+sample = "BRCA"
+
+# Set path
+my.path = file.path("~/TestRun/TCGA-Expression-Gene",sample)
+setwd(my.path)
+
+dataDir = file.path(sample,"data")
+
+files = list.files(dataDir,full.names=TRUE,recursive=T)
