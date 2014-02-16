dc258ed0ce6e3b45e7b2ee1807522772021efcd1
Nitesh Turaga <nitesh.turaga@gmail.com>
2014-02-16 09:00:50 -0500

    feb 16th update

--- 02_explore.R
+++ 02_explore.R
@@ -1,9 +1,14 @@
 # Nitesh Turaga
 # TCGA- Expression-Gene UNC Agilent analysis
 
+
+# Library laod
 library(plyr)
 library(reshape)
+library(limma)
 library(data.table)
+library(BiocInstaller)
+library(gcrma)
 
 
 sample = "BRCA"
@@ -37,6 +42,11 @@ build.expression.matrix = function(list.of.files) {
         f2$Hybridization.REF = NULL    
         f1 = cbind(f1,f2)
     }
+    # Just to make sure the names are cast on rows and columns
+    rownames(f1) = rownames(f1)
+    colnames(f1) = colnames(f1)    
+    # Return a data matrix
+    f1 = data.matrix(f1)
     return(f1)
 }
 
@@ -44,7 +54,47 @@ build.expression.matrix = function(list.of.files) {
 normal_dat = build.expression.matrix(normal)
 cancer_dat = build.expression.matrix(cancer)
 
-rownames(cancer_dat) = rownames(cancer_dat)
-colnames(cancer_dat) = colnames(cancer_dat)
-cancer_data = data.matrix(cancer_dat) 
+#save(normal_dat,cancer_dat,file = "expressionMat.rda",compress =TRUE)
+
+# Phenotype of expression matrix
+phenotype = data.frame(filenames = c(colnames(cancer_dat),colnames(normal_dat)),
+                       status = c(rep(x="Cancer",length(cancer)),rep(x="Normal",length(normal))))
+phenotype$status = as.factor(phenotype$status)
+
+# Design matrix
+design = model.matrix(phenotype$filenames~phenotype$status)
+
+# Expression matrix
+expression_mat = cbind(cancer_dat,normal_dat)
+
+# Linear model fit
+fit = lmFit(expression_mat,design)
+
+# Plot SA of the linear model fit.
+#pdf(file = "fit_plot.pdf")
+#plotSA(fit,main = "Gene level")
+#dev.off()
+summary(fit)
+
+#Seperate fits
+fit = ebayes(fit)
+
+#topTable(fit$p.value[,2], coef="phenotype$statusNormal", adjust="BH")
+
+#class(fit)
+#names(fit)
+
+fit$p.corr = p.adjust(fit$p.value[,2],method="bonferroni")
+
+subset.fit = fit$p.corr[fit$p.corr<=0.0005]
+pdf(file = "heatmap.pdf")
+heatmap(expression_mat[names(subset.fit),])
+dev.off()
+### If FDR does not give a subset of <5000 genes, use a more stringent correction metho
+summary(subset.fit$p.corr)
+# subset.df = subset.fit
+
+
+# library(Biobase)
+# eset = new("ExpressionSet",assayData = expression_mat,phenoData = phenotype,featureData = NULL,annotation = NULL)
 
